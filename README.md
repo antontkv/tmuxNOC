abda141621ad117664aaa4c12cf3a97c9057bdb4
Anton Tuchkov <to.tuchkov@gmail.com>
2020-05-29 13:10:41 +0500

    First commit, install script for tmux
    Script for debian based distros. Removes old version from apt,
    installing new version from source files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# tmuxNOC
+
+tmux config and scripts for people, who have to manage a lot of network
+devices.
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TMUX_VERSION=3.1b
+
+# Removing old tmux installed from apt.
+sudo apt-get -y remove tmux
+# Installing dependencies
+sudo apt-get -y install wget tar libevent-dev ncurses-dev build-essential\
+                        bison pkg-config
+
+# Downloading and unpacking tmux source files
+wget https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/\
+tmux-$TMUX_VERSION.tar.gz
+tar -zxf tmux-$TMUX_VERSION.tar.gz
+rm -f tmux-$TMUX_VERSION.tar.gz
+
+# Building and installing tmux
+cd tmux-$TMUX_VERSION
+./configure
+make && sudo make install
+cd -
+rm -rf tmux-$TMUX_VERSION
