430955e61c433dd78c669903a172569a67f796df
Anton Tuchkov <to.tuchkov@gmail.com>
2020-06-05 23:17:48 +0500

    Setting new line limits, 80 soft, 100 hard
    Just changed some line lingth for better readability.

--- install.sh
+++ install.sh
@@ -5,12 +5,10 @@ TMUX_VERSION=3.1b
 # Removing old tmux installed from apt.
 sudo apt-get -y remove tmux
 # Installing dependencies
-sudo apt-get -y install wget tar libevent-dev ncurses-dev build-essential\
-                        bison pkg-config
+sudo apt-get -y install wget tar libevent-dev ncurses-dev build-essential bison pkg-config
 
 # Downloading and unpacking tmux source files
-wget https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/\
-tmux-$TMUX_VERSION.tar.gz
+wget https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/tmux-$TMUX_VERSION.tar.gz
 tar -zxf tmux-$TMUX_VERSION.tar.gz
 rm -f tmux-$TMUX_VERSION.tar.gz
 
