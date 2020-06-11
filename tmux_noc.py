3a16690c1de5b2131fa449216c315ffc3d5f38c7
Anton Tuchkov <to.tuchkov@gmail.com>
2020-06-11 10:20:21 +0500

    Send lines or characters with delay
    Function to send lines or characters with configurate delay in
    milliseconds.

--- tmux_noc.py
+++ tmux_noc.py
@@ -6,6 +6,17 @@ import json
 import datetime
 from pathlib import Path
 
+class ANSIColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def load_sessions():
     home = str(Path.home())
     with open(f'{home}/tmuxNOC/sessions.json', 'r') as f:
@@ -51,16 +62,16 @@ def connect_telnet(host):
             'tmux',
             'new-window',
             '-n', host,
-            f'PROMPT_COMMAND="telnet {host}";TERM=vt100-w bash --rcfile {home}/tmuxNOC/tmux_noc_bashrc'
+            f'PROMPT_COMMAND="telnet {host}";TERM=vt100-w bash \
+              --rcfile {home}/tmuxNOC/tmux_noc_bashrc'
         ]
     )
     subprocess.run(['tmux', 'source-file', f'{home}/tmuxNOC/telnet_tmux.conf'])
-    # subprocess.run(['tmux', 'send-keys', f'telnet {host}', 'Enter'])
     save_session('telnet', host)
 
-def tmux_send(string):
+def tmux_send(string, conformation_symbol='Enter', target_pane='*'):
     subprocess.run(
-        ['tmux', 'send-keys', string, 'Enter'],
+        ['tmux', 'send-keys', '-t', target_pane, string, f'{conformation_symbol}'],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
@@ -101,15 +112,82 @@ def send_login_pwd(login_number):
     else:
         subprocess.run(['tmux', 'display-message', 'Password prompt not found.'])
 
+def send_with_delay(pane_id):
+    print(f'{ANSIColors.WARNING}What to send? To end list enter a single dot{ANSIColors.ENDC}\n.')
+    commands, s = [], ''
+    while s != '.':
+        s = input()
+        if s != '.':
+            commands.append(s)
+    while True:
+        try:
+            line_delay = input(
+                f'{ANSIColors.WARNING}Enter LINE delay in milliseconds [500]: {ANSIColors.ENDC}'
+            ) or 500
+            line_delay = int(line_delay)
+        except ValueError:
+            print(f'{ANSIColors.FAIL}Enter an integer.{ANSIColors.ENDC}')
+            continue
+        if line_delay < 0:
+            print(f'{ANSIColors.FAIL}Enter a positive integer or 0.{ANSIColors.ENDC}')
+            continue
+        else:
+            break
+    while True:
+        try:
+            character_delay = input(
+                f'{ANSIColors.WARNING}Enter CHARACTER delay in milliseconds [0]: {ANSIColors.ENDC}'
+            ) or 0
+            character_delay = int(character_delay)
+        except ValueError:
+            print(f'{ANSIColors.FAIL}Enter an integer.{ANSIColors.ENDC}')
+            continue
+        if character_delay < 0:
+            print(f'{ANSIColors.FAIL}Enter a positive integer or 0.{ANSIColors.ENDC}')
+            continue
+        else:
+            break
+
+    rows, columns = subprocess.check_output(['stty', 'size']).decode().split()
+    rows = int(rows) - 2
+    rows_offset = 0
+    for index, command in enumerate(commands):
+        subprocess.call('clear', shell=True)
+        if len(commands) > rows:
+            if index > 5:
+                rows_offset = index - 5
+        print('\n'.join(commands[rows_offset:index]))
+        print(f'{ANSIColors.OKGREEN}{ANSIColors.BOLD}{command}{ANSIColors.ENDC}')
+        print('\n'.join(commands[index + 1:rows + rows_offset]))
+
+        if character_delay > 0:
+            characters = list(command)
+            for character in characters:
+                tmux_send(character, '', pane_id)
+                time.sleep(character_delay / 1000)
+            tmux_send('', 'Enter', pane_id)
+        else:
+            tmux_send(command, target_pane=pane_id)
+        if not index + 1 == len(commands):
+            time.sleep(line_delay / 1000)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Connect to telnet or ssh from tmux.')
-    parser.add_argument('type', choices=['login', 'setup_telnet', 'connect_telnet'])
+    parser.add_argument('type', choices=[
+        'login',
+        'send_with_delay',
+        'setup_telnet',
+        'connect_telnet',
+    ])
     parser.add_argument('--login_number', nargs='?')
     parser.add_argument('--host', nargs='?')
+    parser.add_argument('--pane_id', nargs='?')
     args = parser.parse_args()
 
     if args.type == 'login':
         send_login_pwd(args.login_number)
+    elif args.type == 'send_with_delay':
+        send_with_delay(args.pane_id)
     elif args.type == 'setup_telnet':
         setup_telnet()
     elif args.type == 'connect_telnet':
