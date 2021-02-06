fc767ba4d9b18278b9ec24be2c5f3da5696ff28e
Anton Tuchkov <to.tuchkov@gmail.com>
2021-02-06 23:35:36 +0500

    install.sh will now use .deb

--- install.sh
+++ install.sh
@@ -4,43 +4,50 @@ set -e
 set -u
 set -o pipefail
 
-TMUX_VERSION=3.1b
-TMUX_RC=
+TMUX_VERSION=3.2-rc3
 
 is_app_installed() {
   type "$1" &>/dev/null
 }
 
-install_tmux() {
-  printf "Install tmux from source\n"
-  # Installing dependencies
-  sudo apt-get -y install wget tar libevent-dev ncurses-dev build-essential bison pkg-config
+install_dependencies() {
+  sudo apt update
+  sudo apt-get -y install expect git telnet
+}
 
-  # Downloading and unpacking tmux source files
+install_tmux() {
+  # Installing dependencies
   if [ ! -e "$HOME/tmuxNOC/" ]; then
       mkdir "$HOME/tmuxNOC"
   fi
   cd ~/tmuxNOC/
-  wget https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/tmux-$TMUX_VERSION$TMUX_RC.tar.gz
-  tar -zxf tmux-$TMUX_VERSION$TMUX_RC.tar.gz
-  rm -f tmux-$TMUX_VERSION$TMUX_RC.tar.gz
+  sudo apt update
+  sudo apt-get -y install wget libutempter0
+  wget http://ftp.us.debian.org/debian/pool/main/libe/libevent/libevent-2.1-7_2.1.12-stable-1_amd64.deb
+  sudo dpkg -i libevent-2.1-7_2.1.12-stable-1_amd64.deb
+  rm libevent-2.1-7_2.1.12-stable-1_amd64.deb
 
-  # Building and installing tmux
-  cd tmux-$TMUX_VERSION$TMUX_RC
-  ./configure
-  make && sudo make install
-  cd -
-  rm -rf tmux-$TMUX_VERSIO$TMUX_RC
+  # Downloading .deb and installing
+  wget http://ftp.us.debian.org/debian/pool/main/t/tmux/tmux_3.2~rc3-1_amd64.deb
+  sudo dpkg -i tmux_3.2~rc3-1_amd64.deb
+  rm tmux_3.2~rc3-1_amd64.deb
 }
 
+if ! is_app_installed expect || ! is_app_installed git || ! is_app_installed telnet; then
+  printf "\nYou don't have expect, git or telnet installed on the system.\n\
+To install them you'll need sudo rights.\n\n"
+read -p "Press Enter to continue..."
+  install_dependencies
+fi
+
 if ! is_app_installed tmux; then
-  printf "\nTmux is not installed. Version $TMUX_VERSION$TMUX_RC will be build and installed from source.\n\
-To install dependencies for build you need sudo rights.\n\n"
+  printf "\nTmux is not installed. Version $TMUX_VERSION will be downloaded and installed from Debian archives.\n\
+To install you'll need sudo rights.\n\n"
 read -p "Press Enter to continue..."
   install_tmux
 elif ! tmux -V | grep -q $TMUX_VERSION; then
   printf "\nWARNING: You have$(tmux -V | sed s/tmux//) tmux version installed.\nThis project was tested on \
-$TMUX_VERSION$TMUX_RC tmux version. So something may be broken.\nYou can cancel this script and remove tmux from the \
+$TMUX_VERSION tmux version. So something may be broken.\nYou can cancel this script and remove tmux from the \
 system. Then run the script again, it will install needed version of tmux.\n\n"
   read -p "Press Enter to continue..."
 fi
