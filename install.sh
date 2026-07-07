41d574826828416f6598c4c82c8461a3e1bcef38
Anton Tuchkov <to.tuchkov@gmail.com>
2026-07-07 19:54:08 +0500

    Remove appimage install and minimal version check from installer

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ set -e
 set -u
 set -o pipefail
 
-TMUX_VERSION=3.2a
-
 is_app_installed() {
   type "$1" &>/dev/null
 }
@@ -26,13 +24,7 @@ install_tmux() {
   fi
   cd ~/tmuxNOC/
   if is_app_installed apt; then
-    sudo apt update
-    sudo apt-get -y install wget
-
-    # Downloading .appimage and installing it.
-    wget https://github.com/antontkv/tmux-appimage/releases/download/$TMUX_VERSION/tmux-$TMUX_VERSION-x86_64.appimage
-    chmod +x tmux-$TMUX_VERSION-x86_64.appimage
-    sudo mv tmux-$TMUX_VERSION-x86_64.appimage /usr/local/bin/tmux
+    sudo apt-get install tmux
   elif is_app_installed pacman; then
     sudo pacman -Sy tmux
   else
@@ -49,16 +41,10 @@ read -p "Press Enter to continue..."
 fi
 
 if ! is_app_installed tmux; then
-  printf "\nTmux is not installed. Version $TMUX_VERSION will be downloaded and installed from \
-https://github.com/antontkv/tmux-appimage/releases or from pacman if on Arch.\n\
+  printf "\nTmux is not installed. Package manager will be used to install it.\n\
 To install you'll need sudo rights.\n\n"
 read -p "Press Enter to continue..."
   install_tmux
-elif ! tmux -V | grep -q $TMUX_VERSION; then
-  printf "\nWARNING: You have$(tmux -V | sed s/tmux//) tmux version installed.\nThis project was tested on \
-$TMUX_VERSION tmux version. So something may be broken.\nYou can cancel this script and remove tmux from the \
-system. Then run the script again, it will install needed version of tmux.\n\n"
-  read -p "Press Enter to continue..."
 fi
 
 if [ -e "$HOME/.tmux.conf" ]; then
